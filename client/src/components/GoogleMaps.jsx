cd34765d1211dc711190d7cc575195ec5c8372ba
Sterling Deng <sterlingdeng@gmail.com>
2018-10-14 22:29:36 -0700

    changes made to improve how markers are rendered in the map

--- client/src/components/GoogleMaps.jsx
+++ client/src/components/GoogleMaps.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+
 const loadjs = require("loadjs");
 
 export default class GoogleMaps extends Component {
@@ -6,56 +7,22 @@ export default class GoogleMaps extends Component {
     super(props);
     this.state = {
       map: undefined,
-      apiLoaded: false,
-      currentInfoWindow: undefined,
-      currentMarkers: []
+      apiLoaded: false
     };
+    this.currentInfoWindow = undefined;
+    this.businessData = {};
+    this.markers = [];
     this.initMap = this.initMap.bind(this);
-    this._renderMarkers = this._renderMarkers.bind(this);
     this._createMarkers = this._createMarkers.bind(this);
-    this._deleteMarkers = this._deleteMarkers.bind(this);
-  }
-
-  _createMarkers() {
-    if (this.props.yelpBusinessList === undefined) {
-      return;
-    }
-
-    for (let business in this.props.yelpBusinessList.MappedBusinessStruct) {
-      const businessData = this.props.yelpBusinessList.MappedBusinessStruct[
-        business
-      ];
-      this._renderMarkers(businessData);
-    }
-  }
-
-  componentWillMount() {
-    loadjs(
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyDxEICqms-JYUOkuSKpakoWhIMlJ2MQJ7U&callback=initMap",
-      () => {
-        this.setState({ apiLoaded: true });
-      }
-    );
-    window.initMap = this.initMap;
-    window._createMarkers = this._createMarkers;
-  }
-
-  componentDidUpdate() {
-    if (this.state.currentMarkers.length <= 20) {
-      this._createMarkers();
-    } else {
-      this._deleteMarkers();
-    }
+    this.updateBusinessList = this.updateBusinessList.bind(this);
   }
 
   initMap() {
     const google = window.google;
-
     let map = new google.maps.Map(document.getElementById("map"), {
       zoom: 12,
       gestureHandling: "greedy"
     });
-
     this.setState({ map: map });
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
@@ -69,71 +36,108 @@ export default class GoogleMaps extends Component {
       );
     }
   }
+  componentWillMount() {
+    loadjs(
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyDxEICqms-JYUOkuSKpakoWhIMlJ2MQJ7U&callback=initMap",
+      () => {
+        this.setState({ apiLoaded: true });
+      }
+    );
+    window.initMap = this.initMap;
+  }
+  updateBusinessList() {
+    if (this.props.yelpBusinessList === undefined) {
+      return;
+    }
+    // If the api hasn't loaded, do not attempt to apply markers to map.
+    if (!this.state.apiLoaded) {
+      return;
+    }
 
-  _deleteMarkers() {
-    this.state.currentMarkers.forEach(marker => {
-      marker.setMap(null);
-    });
-    this.setState({
-      currentMarkers: []
+    if (Object.values(this.businessData).length !== 0) {
+      this.businessData = {};
+    }
+
+    for (let business in this.props.yelpBusinessList) {
+      const businessData = this.props.yelpBusinessList[business];
+      this.businessData[business] = businessData;
+    }
+  }
+
+  _createMarkers() {
+    // First, render the marker on the map
+    const google = window.google;
+
+    Object.values(this.businessData).forEach((businessData, idx) => {
+      const marker = new google.maps.Marker({
+        position: {
+          lat: businessData.coordinates.latitude,
+          lng: businessData.coordinates.longitude
+        },
+        title: "hello",
+        content: `
+          <div id="content">
+            <div class="marker-business-name">${businessData.name}</div>
+            <div class="marker-stars">${businessData.rating}</div>
+            <div class="marker-dollar-signs">${businessData.price}</div>
+            <div class="marker-address">${
+              businessData.location.display_address[0]
+            }\n${businessData.location.display_address[1]}</div>
+            <img class="marker-image" src=${
+              businessData.image_url
+            } alt="restaurant picture"/>
+          </div>`,
+        label: `${idx + 1}`
+      });
+      this.markers.push(marker);
+      this._renderMarkers(marker);
     });
   }
 
-  //latlng = {lat: lat, lng: lng}
-  _renderMarkers(business) {
-    // If the api hasn't loaded, do not attempt to apply markers to map.
-    if (!this.state.apiLoaded) {
-      return false;
-    }
-    // First, render the marker on the map
+  _renderMarkers(marker) {
     const google = window.google;
-    const marker = new google.maps.Marker({
-      position: {
-        lat: business.coordinates.latitude,
-        lng: business.coordinates.longitude
-      },
-      title: "hello"
-    });
     marker.setMap(this.state.map);
-
-    this.setState(
-      {
-        currentMarkers: [...this.state.currentMarkers, marker]
-      },
-      console.log(this.state.currentMarkers)
-    );
-
-    // Next, render the content inside the marker. This should somewhat resemble
-    // how yelp renders information in their markers.
-    const contentString = `
-    <div id="content">
-      <div class="marker-business-name">${business.name}</div>
-      <div class="marker-stars">${business.rating}</div>
-      <div class="marker-dollar-signs">${business.price}</div>
-      <div class="marker-address">${business.location.display_address[0]}\n${
-      business.location.display_address[1]
-    }</div>
-      <img class="marker-image" src=${
-        business.image_url
-      } alt="restaurant picture"/>
-    </div>`;
-
     const infoWindow = new google.maps.InfoWindow({
-      content: contentString
+      content: marker.content
     });
-
     marker.addListener("click", () => {
       // if there is another InfoWindow open, close it first before rendering the new one
-      if (this.state.currentInfoWindow) {
-        this.state.currentInfoWindow.close();
+      if (this.currentInfoWindow) {
+        this.currentInfoWindow.close();
       }
       // Open the newly clicked infoWindow
       infoWindow.open(this.state.map, marker);
       // setState to keep track of which infoWindow is open
-      this.setState({
-        currentInfoWindow: infoWindow
-      });
+      this.currentInfoWindow = infoWindow;
     });
+  }
+
+  equality(obj1, obj2) {
+    if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+      return false;
+    }
+
+    const equalityTest = Object.keys(obj1).every(business => {
+      return Object.prototype.hasOwnProperty.call(obj2, business);
+    });
+
+    return equalityTest;
+  }
+
+  componentDidUpdate() {
+    const isEqual = this.equality(
+      this.businessData,
+      this.props.yelpBusinessList
+    );
+
+    if (!isEqual) {
+      this.markers.forEach(marker => {
+        marker.setMap(null);
+      });
+      this.markers = [];
+      this.updateBusinessList();
+      this._createMarkers();
+    }
   }
 
   render() {
