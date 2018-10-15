8f8b7c381e8314ee50a2e9271186281f3060bf35
Sterling Deng <sterlingdeng@gmail.com>
2018-10-14 22:32:01 -0700

    changed app container from class to functional component

--- client/src/components/AppContainer.jsx
+++ client/src/components/AppContainer.jsx
@@ -1,43 +1,42 @@
-import React, { Component } from "react";
+import React from "react";
 import Sidebar from "./Sidebar";
 import GoogleMaps from "./GoogleMaps";
 import ListDisplay from "./ListDisplay";
 import NomineeList from "./NomineeList";
 
-export default class AppContainer extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className="container-fluid">
-        <div className="row">
-          <Sidebar
-            wsconn={this.props.wsconn}
-            username={this.props.username}
-            messages={this.props.messages}
-            className="sidebar"
-          />
-          <ListDisplay
-            yelpBusinessList={this.props.yelpBusinessList}
-            handleNominateClick={this.props.handleNominateClick}
-            yelpOffset={this.props.yelpOffset}
-            handleBackBusinessList={this.props.handleBackBusinessList}
-            handleNextBusinessList={this.props.handleNextBusinessList}
-          />
+const AppContainer = props => {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <Sidebar
+          wsconn={props.wsconn}
+          username={props.username}
+          messages={props.messages}
+          className="sidebar"
+        />
+        <ListDisplay
+          yelpBusinessList={props.yelpBusinessList}
+          handleNominateClick={props.handleNominateClick}
+          yelpOffset={props.yelpOffset}
+          handleBackBusinessList={props.handleBackBusinessList}
+          handleNextBusinessList={props.handleNextBusinessList}
+        />
 
-          <div className="col">
-            <GoogleMaps
-              location={this.props.location}
-              getLocation={this.props.getLocation}
-              yelpBusinessList={this.props.yelpBusinessList}
+        <div className="col">
+          <GoogleMaps
+            location={props.location}
+            getLocation={props.getLocation}
+            yelpBusinessList={props.yelpBusinessList}
+          />
+          <div className="row">
+            <NomineeList
+              nomineeList={props.nomineeList}
+              readyUp={props.readyUp}
             />
-            <div className="row">
-              <NomineeList nomineeList={this.props.nomineeList} />
-            </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+export default AppContainer;
